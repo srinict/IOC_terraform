bc35a19b790457bf4994cd901e977deb0270c43f
srinict <srinict@gmail.com>
2024-03-21 17:40:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,14 @@ terraform {
   }
 
 provider "azurerm" {
-      features {}
+  subscription_id = "58b00871-09b2-47c6-927a-5add9be6512a"
+  tenant_id = "06b141cd-7ff5-4a88-91cf-468c3061aa3d"
+  client_id = "b5ed2d0e-ed93-4aff-ad59-ff33e07ad051"
+  client_secret = "tZb8Q~hxJldaime_mQ~HxVJ~cE4xPcnpZ1pk4az."
+    features {}
   use_cli = false
 }
+
 
 resource "azurerm_resource_group" "rgterraform" {
   name     ="rgtfpoctest" 
